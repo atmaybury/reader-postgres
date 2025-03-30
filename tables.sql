94ae0c80a19534ca32d6c3142bb8bec8731e680a
Andrew Maybury <atmaybury@protonmail.com>
2025-03-30 20:41:37 +1100

    added subscriptions

--- tables.sql
+++ tables.sql
@@ -1,9 +1,21 @@
 
--- public.users definition
+-- USERS
 CREATE TABLE public.users (
 	id serial4 NOT NULL,
 	username text NOT NULL,
 	email text NOT NULL,
 	"password" text NOT NULL,
 	CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+-- SUBSCRIPTIONS
+CREATE TABLE public.subscriptions (
+	id serial4 NOT NULL,
+	user_id int NOT NULL,
+	title text NOT NULL,
+	url text NOT NULL,
+	CONSTRAINT subscriptions_pkey PRIMARY KEY (id),
+	CONSTRAINT subscriptions_user_id_fkey 
+		FOREIGN KEY (user_id)
+		REFERENCES public.users(id)
 );
