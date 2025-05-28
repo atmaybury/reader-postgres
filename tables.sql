4c7097a7a27f494061758bd43936ef38f7834efc
Andrew Maybury <atmaybury@protonmail.com>
2025-05-28 20:24:28 +1000

    updated subscriptions/feeds

--- tables.sql
+++ tables.sql
@@ -8,14 +8,25 @@ CREATE TABLE public.users (
 	CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
+-- FEEDS
+CREATE TABLE public.feeds (
+	url text NOT NULL,
+	title text NOT NULL,
+	last_checked timestamptz NOT NULL DEFAULT NOW(),
+	CONSTRAINT feeds_pkey PRIMARY KEY (url)
+);
+
 -- SUBSCRIPTIONS
 CREATE TABLE public.subscriptions (
 	id serial4 NOT NULL,
 	user_id int NOT NULL,
-	title text NOT NULL,
-	url text NOT NULL,
+	feed_url string NOT NULL,
 	CONSTRAINT subscriptions_pkey PRIMARY KEY (id),
 	CONSTRAINT subscriptions_user_id_fkey 
 		FOREIGN KEY (user_id)
-		REFERENCES public.users(id)
+		REFERENCES public.users(id),
+	CONSTRAINT subscriptions_feed_url_fkey 
+		FOREIGN KEY (feed_url)
+		REFERENCES public.feeds(url)
 );
+
